bfd9d12034d835c3275bc644726a41333233d46e
vishesh0123 <visheshsachdev04@gmail.com>
2022-06-25 14:34:19 +0530

    contract update

--- contracts/University.sol
+++ contracts/University.sol
@@ -23,6 +23,14 @@ contract University {
     mapping(address => uint256) lendingPool;
     //Total Liquidity deposited to lending pool
     uint256 public totalLendingReserve;
+    //Collateral tracker of tokenIssued
+    mapping(address => uint256) collateralReserve;
+    //Borrow amount tracker
+    mapping(address => uint256) borrowTracker;
+    //Total Borrowed amount
+    uint256 public totalBorrow;
+
+    address public usdt;
 
     // monthly intrest in percentage
     uint32 public monthlyIntrest;
@@ -46,7 +54,7 @@ contract University {
 
     function deposit(uint256 amount) public returns (bool) {
         require(msg.sender == admin || approvalStatus[msg.sender] == true);
-        bool status = IERC20(msg.sender).transferFrom(
+        bool status = IERC20(usdt).transferFrom(
             msg.sender,
             address(this),
             amount
@@ -54,6 +62,46 @@ contract University {
         require(status == true);
         lendingPool[msg.sender] = lendingPool[msg.sender] + amount;
         totalLendingReserve = totalLendingReserve + amount;
+        reservesList.push(msg.sender);
         return (true);
+    }
+
+    function withdraw(uint256 amount) public returns (bool) {
+        require(msg.sender == admin || approvalStatus[msg.sender] == true);
+        require(totalLendingReserve >= amount);
+        lendingPool[msg.sender] = lendingPool[msg.sender] - amount;
+        totalLendingReserve = totalLendingReserve - amount;
+        bool status = IERC20(usdt).transfer(msg.sender, amount);
+        require(status == true);
+        return true;
+    }
+
+    function borrow(uint256 amount, uint32 months) returns (bool) {
+        require(msg.sender != admin);
+        require(approvalStatus[msg.sender] == true);
+        require(borrowTracker[msg.sender] == 0);
+        require(lendingReserve >= amount);
+        uint256 collateral = (amount) + ((amount * 20) / 100);
+        bool status = IERC20(tokenIssued).transferFrom(
+            msg.sender,
+            address(this),
+            collateral
+        );
+        require(status == true);
+        collateralReserve[msg.sender] =
+            collateralReserve[msg.sender] +
+            collateral;
+        borrowTracker[msg.sender] = amount;
+        totalBorrow = totalBorrow + amount;
+        totalLendingReserve = totalLendingReserve - amount;
+        bool borrowstatus = IERC20(usdt).transfer(msg.sender, amount);
+        require(borrowstatus == true);
+        return true;
+    }
+
+    function repay() public returns (bool) {
+        require(msg.sender != admin);
+        require(approvalStatus[msg.sender] == true);
+        require(borrowTracker[msg.sender] != 0);
     }
 }
